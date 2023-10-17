5d1a19d43d815697177c204d1535f790b0fb8f09
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-17 19:09:35 +0200

    Add: html sctructure, numbers gen on click

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// #Js Simon
+// variabili
+
+const randomNumbersArray = getArrayOfRandomNumberBetween(1, 100, 5)
+console.log(randomNumbersArray)
+
+const startBtnDOMElement = document.querySelector('.start-btn')
+console.log(startBtnDOMElement)
+
+
+// Al click stampo i numeri in pagina
+
+startBtnDOMElement.addEventListener('click', function(){
+    printInPage('.random-numbers', randomNumbersArray)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// funzioni
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+function getArrayOfRandomNumberBetween (min,max,range) {
+    const numbersArray = [];
+
+    while ( numbersArray.length < range){
+        const randomNumber = getRandomIntInclusive(min,max);
+        if (numbersArray.includes(randomNumber) === false){
+            numbersArray.push(randomNumber)
+        }
+    }
+    return numbersArray
+}
+
+function printInPage (className, text){
+    const element = document.querySelector(className)
+    element.innerHTML = text
+}
